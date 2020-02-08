2bdab42304143d5a08c29c041c47c0c85d78452c
Nikita_Gipich <Nikita_Gipich@epam.com>
2020-02-08 23:32:10 +0400

    Fixed extra ',' symbol

--- src/UNStaistic/UNStaistic.DB/dwh/Tables/FactPopulation.sql
+++ src/UNStaistic/UNStaistic.DB/dwh/Tables/FactPopulation.sql
@@ -3,10 +3,10 @@
 	[FactPopulation]	INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
 	[FactPopulationKey]	INT NOT NULL DEFAULT -1,
 	[DimGeographyKey]	INT NOT NULL DEFAULT -1,
-	[DimDateKey]	INT NOT NULL DEFAULT -1,
-	[Population]	BIGINT NOT NULL,
-	[sysCreatedAt]	DATETIME DEFAULT GETUTCDATE(),
-	[sysChangedAt]	DATETIME DEFAULT GETUTCDATE(),
-	[sysChangedBy]	INT NOT NULL DEFAULT -1,
-	[sysCreatedBy]	INT NOT NULL DEFAULT -1,
+	[DimDateKey]		INT NOT NULL DEFAULT -1,
+	[Population]		BIGINT NOT NULL,
+	[sysCreatedAt]		DATETIME DEFAULT GETUTCDATE(),
+	[sysChangedAt]		DATETIME DEFAULT GETUTCDATE(),
+	[sysChangedBy]		INT NOT NULL DEFAULT -1,
+	[sysCreatedBy]		INT NOT NULL DEFAULT -1
 )
