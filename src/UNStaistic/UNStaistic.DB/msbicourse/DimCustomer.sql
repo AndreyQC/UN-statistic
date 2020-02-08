92224ceaa258b69bb302f3289330fbab8a71bb2e
Andrey Potapov <Andrey_Potapov@epam.com>
2020-02-08 21:09:59 +0400

    intermidiate commit

---
+++ src/UNStaistic/UNStaistic.DB/msbicourse/DimCustomer.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [msbicourse].[DimCustomer]
+( 
+     [DimCustomerKey] [int] IDENTITY(1,1) NOT NULL,
+     [CustomerBK] [int]  NOT NULL, 
+     [companyname] [nvarchar](40) NOT NULL,
+     [contactname] [nvarchar](30) NOT NULL,
+     [contacttitle] [nvarchar](30) NOT NULL,
+     [address] [nvarchar](60) NOT NULL,
+     [city] [nvarchar](15) NOT NULL,
+     [region] [nvarchar](15) NULL,
+     [postalcode] [nvarchar](10) NULL,
+     [country] [nvarchar](15) NOT NULL,
+     [phone] [nvarchar](24) NOT NULL,
+     [fax] [nvarchar](24) NULL,
+     [sysCreatedAt] DATETIME DEFAULT GETUTCDATE(),
+     [sysChangedAt] DATETIME DEFAULT GETUTCDATE(),
+     [sysChangedBy] INT NOT NULL DEFAULT -1,
+     [sysCreatedBy] INT NOT NULL DEFAULT -1, 
+    CONSTRAINT [PK_DimCustomer] PRIMARY KEY CLUSTERED ([DimCustomerKey])
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'business key of entry (custid)',
+    @level0type = N'SCHEMA',
+    @level0name = N'msbicourse',
+    @level1type = N'TABLE',
+    @level1name = N'DimCustomer',
+    @level2type = N'COLUMN',
+    @level2name = N'CustomerBK'
---
+++ src/UNStaistic/UNStaistic.DB/msbicourse/staging_Customer.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [msbicourse].[staging_Customer]
+(
+ [custid] [int] IDENTITY(1,1) NOT NULL,
+ [companyname] [nvarchar](40) NOT NULL,
+ [contactname] [nvarchar](30) NOT NULL,
+ [contacttitle] [nvarchar](30) NOT NULL,
+ [address] [nvarchar](60) NOT NULL,
+ [city] [nvarchar](15) NOT NULL,
+ [region] [nvarchar](15) NULL,
+ [postalcode] [nvarchar](10) NULL,
+ [country] [nvarchar](15) NOT NULL,
+ [phone] [nvarchar](24) NOT NULL,
+ [fax] [nvarchar](24) NULL,
+)
