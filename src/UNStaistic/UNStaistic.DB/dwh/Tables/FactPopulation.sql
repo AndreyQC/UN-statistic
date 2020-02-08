ff897101ee8bc30d56f4aa70581af6bc99c3b8f2
Nikita_Gipich <Nikita_Gipich@epam.com>
2020-02-08 18:55:20 +0400

    Task-307 was completed created a new dwh.FactPopulation table

---
+++ src/UNStaistic/UNStaistic.DB/dwh/Tables/FactPopulation.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dwh].[FactPopulation]
+(
+	[FactPopulation]	INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
+	[FactPopulationKey]	INT NOT NULL DEFAULT -1,
+	[DimGeographyKey]	INT NOT NULL DEFAULT -1,
+	[DimDateKey]	INT NOT NULL DEFAULT -1,
+	[Population]	BIGINT NOT NULL,
+	[sysCreatedAt]	DATETIME DEFAULT GETUTCDATE(),
+	[sysChangedAt]	DATETIME DEFAULT GETUTCDATE(),
+	[sysChangedBy]	INT NOT NULL DEFAULT -1,
+	[sysCreatedBy]	INT NOT NULL DEFAULT -1,
+)
