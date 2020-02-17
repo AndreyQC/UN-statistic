d9c09e4647c613acc649c4ba6926c1c6a8899b11
Andrey Potapov <Andrey_Potapov@epam.com>
2020-02-17 11:33:34 +0400

    lecture was perfromed

---
+++ src/UNStatistic/UNStatistic.DB/dbo/tables/sysssislog.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[sysssislog](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[event] [sysname] NOT NULL,
+	[computer] [nvarchar](128) NOT NULL,
+	[operator] [nvarchar](128) NOT NULL,
+	[source] [nvarchar](1024) NOT NULL,
+	[sourceid] [uniqueidentifier] NOT NULL,
+	[executionid] [uniqueidentifier] NOT NULL,
+	[starttime] [datetime] NOT NULL,
+	[endtime] [datetime] NOT NULL,
+	[datacode] [int] NOT NULL,
+	[databytes] [image] NULL,
+	[message] [nvarchar](2048) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
