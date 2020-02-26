70da9cc723199e62178e76dcbc072afb2cd3123a
Andrey Potapov <Andrey_Potapov@epam.com>
2020-02-26 21:59:44 +0400

    added objects for new Master package and for logging

---
+++ src/UNStatistic/UNStatistic.DB/dm/views/vw_DimDate.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vw_DimDate]
+    AS 
+    
+    SELECT 
+        [DateSK]
+        ,[DateBK]
+        ,[CalendarMonth] 
+        ,[CalendarMonthLongName] 
+        ,[CalendarMonthShortName] 
+        ,[CalendarQuarter] 
+        ,[CalendarQuarterShortName] 
+        ,[CalendarYear]
+        ,[CalendarYearMonth]
+        ,[CalendarYearQuarter]
+        ,[DayLongName] 
+        ,[DayShortName] 
+        ,[DayWeekNumber]
+    FROM dwh.DimDate
