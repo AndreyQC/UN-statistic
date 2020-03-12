76e6d29ad3cf9e062dc9fdf54acc9a9ecc3491c3
Andrey Potapov <Andrey_Potapov@epam.com>
2020-03-12 12:41:19 +0400

    lecture was perfromed

---
+++ src/UNStatistic/UNStatistic.DWH.Azure/adf/tables/Configuration.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [adf].[Configuration] (
+    [ConfigurationId]    INT            IDENTITY (1, 1) NOT NULL,
+    [ConfigurationKey]   NVARCHAR (150) NOT NULL,
+    [ConfigurationValue] NVARCHAR (500) NOT NULL,
+    [sysCreatedAt]       DATETIME       CONSTRAINT [DF_Configuration_sysCreatedAt] DEFAULT (getutcdate()) NULL,
+    [sysChangedAt]       DATETIME       CONSTRAINT [DF_Configuration_sysChangedAt] DEFAULT (getutcdate()) NULL,
+    [sysCreatedBy]       INT            CONSTRAINT [DF_Configuration_sysCreatedBy] DEFAULT ((-1)) NULL,
+    [sysChangedBy]       INT            CONSTRAINT [DF_Configuration_sysChangedBy] DEFAULT ((-1)) NULL,
+    CONSTRAINT [PK_Configuration] PRIMARY KEY CLUSTERED ([ConfigurationId] ASC),
+    CONSTRAINT [UQ_ConfigurationKey] UNIQUE NONCLUSTERED ([ConfigurationKey] ASC)
+);
+
+
+
---
+++ src/UNStatistic/UNStatistic.DWH.Azure/seed-configuration.sql
@@ -0,0 +1,25 @@
+﻿
+DECLARE @JSON  NVARCHAR(MAX);
+
+SELECT @JSON = N'[
+{
+"ConfigurationKey": "container",
+"ConfigurationValue": "unstatisticsourcedata"
+},
+{
+"ConfigurationKey": "UNCrimepath",
+"ConfigurationValue": "UNCrime"
+},
+{
+"ConfigurationKey": "Newparam",
+"ConfigurationValue": "nevalue"
+}
+]'
+
+
+
+EXEC [adf].[usp_Configuration_Seed] @JSON
+
+SELECT * FROM [adf].[Configuration]
+
+EXEC [adf].[usp_Configuration_Get]
