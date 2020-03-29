0d082c93ce18c333393ac85a58ec28be3986467f
denis_priadilshchikov <funquasar@gmail.com>
2020-03-29 14:22:18 +0400

    Task 329 was completed. Created table [staging].[population_unpivoted]

---
+++ src/UNStatistic/UNStatistic.DWH.Azure/staging/Tables/population_unpivoted.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [staging].[population_unpivoted]
+(
+	[RowId]	INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
+	[Year]	CHAR(4)  NOT NULL,
+	[CountryCode]	CHAR(3) NOT NULL,
+	[Population]	BIGINT NOT NULL,
+    [sysCreatedAt] datetime
+        CONSTRAINT [DF_population_unpivoted_sysCreatedAt] 
+            DEFAULT GETUTCDATE(),
+    [sysChangedAt] [DateTime]
+        CONSTRAINT [DF_population_unpivoted_sysChangedAt] 
+            DEFAULT GETUTCDATE(),
+    [sysCreatedBy] int NOT NULL
+        CONSTRAINT [DF_population_unpivoted_sysCreatedBy] 
+            DEFAULT -1,
+    [sysChangedBy] int NOT NULL
+        CONSTRAINT [DF_population_unpivoted_sysChangedBy] 
+            DEFAULT -1
+)
